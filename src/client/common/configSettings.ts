8c6f34186a2ac3ca17feaa24b21a83e86f11aded
Don Jayamanne <don.jayamanne@yahoo.com>
2016-11-30 16:53:06 +1100

    removed jupyter settings

--- src/client/common/configSettings.ts
+++ src/client/common/configSettings.ts
@@ -18,7 +18,6 @@ export interface IPythonSettings {
     unitTest: IUnitTestSettings;
     autoComplete: IAutoCompeteSettings;
     terminal: ITerminalSettings;
-    jupyter: JupyterSettings;
     sortImports: ISortImportSettings;
     workspaceSymbols: IWorkspaceSymbolSettings;
     envFile: string;
@@ -284,12 +283,6 @@ export class PythonSettings extends EventEmitter implements IPythonSettings {
             launchArgs: []
         };
 
-        this.jupyter = pythonSettings.get<JupyterSettings>('jupyter');
-        // Support for travis
-        this.jupyter = this.jupyter ? this.jupyter : {
-            appendResults: true, defaultKernel: '', startupCode: []
-        };
-
         this.emit('change');
     }
 
@@ -319,7 +312,6 @@ export class PythonSettings extends EventEmitter implements IPythonSettings {
     public autoComplete: IAutoCompeteSettings;
     public unitTest: IUnitTestSettings;
     public terminal: ITerminalSettings;
-    public jupyter: JupyterSettings;
     public sortImports: ISortImportSettings;
     public workspaceSymbols: IWorkspaceSymbolSettings;
 }
